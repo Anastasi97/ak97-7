df5460128589db9e708523b02fd135c2cf7e460e
Anastasi <vashpopo97@mail.ru>
2023-04-10 17:22:55 +0300

    Изменила названия

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,7 @@
 public class CreditPaymentService {
-        public double calculate(double a, double b, double c) {
-            double monthlyPercent = c / 100 / 12;
-            double result = (a * monthlyPercent) / ( 1 - Math.pow ( 1 + monthlyPercent, - b)) ;
+        public double calculate(double creditAmount, double termInMonths, double percent) {
+            double monthlyPercent = percent / 100 / 12;
+            double result = (creditAmount * monthlyPercent) / ( 1 - Math.pow ( 1 + monthlyPercent, - termInMonths)) ;
             return (int) result;
         }
     }
